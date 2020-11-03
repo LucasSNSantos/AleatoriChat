c2b0a4cdeeb9ca0f59d50ab474382f46d57acc94
Pedro Felipe De Oliveira Facundes <pedro.f.facundes1@gmail.com>
2020-11-03 00:03:23 -0300

    Finished Landing-Page, using nav bar from davi's branch

---
+++ web/aleatori/src/Components/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './NavBar.css';
+
+
+
+function NavBar(){
+    return(
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    
+                        <h1 id="NavBar-Header">
+                            Aleatori Chat
+                            <span></span>
+                            <span></span>
+                        </h1>
+                    
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
