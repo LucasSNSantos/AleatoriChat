308768571117c5f0d6c4f562a89af8ea97b6c4f9
DaviBMMGallo <32436536+Lhimbo@users.noreply.github.com>
2020-11-05 21:58:53 -0300

    Fixed react scripts error

--- web/aleatori/src/index.tsx
+++ web/aleatori/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Landing from './Login';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing/>
+    <App/>
     </React.StrictMode>,
   document.getElementById('root')
 );
--- web/aleatori/src/routes.tsx
+++ web/aleatori/src/routes.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Landing from './PathHttp/Landing';
 import Login from './PathHttp/Login';
 import Register from './PathHttp/Register';
 import { BrowserRouter, Switch , Route } from 'react-router-dom';
@@ -9,7 +8,6 @@ function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Landing}/>
                 <Route path="/login" exact component={Login}/>
                 <Route path="/MainPage" exact component={MainPage}/>
                 <Route path="/createAccount" exact component={Register}/>
