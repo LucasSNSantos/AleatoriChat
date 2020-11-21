f3e4a11c08755ddcc86a2489198a84b96084e1b2
DaviBMMGallo <32436536+Lhimbo@users.noreply.github.com>
2020-11-21 19:09:55 -0300

    Added some graph classes
    added Graph, Vertex, Edge, Adjlist;
    Modified some design { MainPage, Register }

---
+++ backend/src/Graph/Classes/Vertex.ts
@@ -0,0 +1,15 @@
+class Vertex<obj>
+{
+    id:number;
+    Label:string;
+    objec:obj;
+
+    constructor(id: number, label:string){
+        this.id = id;
+        this.Label = label;
+    }
+
+
+}
+
+export default Vertex;
