a94001ffadd95a31f7c857f2a266c6d2874f632e
Goblin_Layer <49722276+GoblinLayer@users.noreply.github.com>
2020-11-27 20:36:33 -0300

    Mail
    Ta funcionando ate onde eu sei

--- backend/src/Controllers/UserController.ts
+++ backend/src/Controllers/UserController.ts
@@ -1,36 +1,28 @@
 import {Request,Response} from 'express';
-import * as yup from 'yup'
+//import * as yup from 'yup'
 import db from '../Database/connection';
+const nodemailer = require("nodemailer");
 
 export default {
 
     async Show(req:Request, res:Response) 
     {
-        try
-        {
-            const user = await db('tb_user').select('*');
-            return res.status(200).json(user);
-        }catch(error)
-        {
-            console.log(error)
-        }
+        const user = await db('tb_user').select('*');
+        //console.log(user);
+        return res.status(200).json(user);
     },
 
     async index(req: Request, res:Response){
             const { id } = req.params;
 
-           try
-            {
-                const user = await db('tb_user').select('*').where('id',id); 
-                return res.status(200).json(user);
-            }catch(error)
-            {
-                return res.status(404).send("Erro");
-            }
+            const user = await db('tb_user').select('*').where('id',id); 
+            return res.status(200).json(user);
     },
 
     async create(request: Request, response:Response){
-        const { username,user_password,user_email,description} = request.body;
+        const { username,user_password,user_email,description} = request.body
+        console.log(request.body)
+
         const data = {
             username: username,
             user_password:user_password,
@@ -38,24 +30,50 @@ export default {
             securitykey:"0000",
             description:description
         }
-        /*const schema = yup.object().shape({
-            username: yup.string().required(),
-            user_password: yup.string().required(),
-            user_email:yup.string().required(),
-            securitykey:yup.string().required().min(4),
-            description:yup.string().required(),
-        })
+        // const schema = yup.object().shape({
+        //     username: yup.string().required(),
+        //     user_password: yup.string().required(),
+        //     user_email:yup.string().required(),
+        //     securitykey:yup.string().required().min(4),
+        //     description:yup.string().required(),
+        // })
 
-        await schema.validate(data,{
-            abortEarly:false
-        })
-        */
+        // await schema.validate(data,{
+        //     abortEarly:false
+        // })
+        
         try{      
             await db('tb_user').insert(data);
             //trigger de cadastro
         
             if(await db('tb_user').select('username').where('username',data.username)){
-                return response.status(201).send('OK, cadastrado.');            
+                
+                //Defining mailer
+                let transporter = nodemailer.createTransport({
+                    host: "smtp.gmail.com",
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                      user: "a", // Sender Email address
+                      pass: "b", // Sender Email password
+                    },
+                    tls: {
+                        // Fix for rejection because of localhost
+                        rejectUnauthorized: false
+                      }
+                    
+                  });
+                
+                  // Send mail (provavelmente rolava fzr uma classe pra deixar bonito but it's life)
+                  let info = await transporter.sendMail({
+                    from: `"AleatoriChat " <COLOCAR EMAIL DO ALEATORI AQUI>`, // Render address
+                    to: `${data.user_email}`, // Receivers
+                    subject: "Hello ✔", // Title
+                    text: "Hello world?", // Plaint text email
+                    html: "<b>Hello world?</b>", // html for styling the email
+                  });
+
+                return response.status(201).send('Você foi cadastrado com sucesso. Bem vindo ao AleatoriChat!.');            
             }
         }catch(error){
             //Pegou a exception do banco ---
--- backend/src/Services/mail.ts
+++ backend/src/Services/mail.ts
@@ -50,5 +50,4 @@ class Mail
 
 
 }
-
 export default new Mail;
