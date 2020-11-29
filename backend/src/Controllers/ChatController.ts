2b96f367d07ffd92e50352ddd458b5b804ac759d
Lucas Souza <souzacontato18@gmail.com>
2020-11-29 00:21:36 -0300

    Create chatController

---
+++ backend/src/Controllers/ChatController.ts
@@ -0,0 +1,34 @@
+import {Request,Response} from 'express';
+import db from '../Database/connection';
+
+
+export default {
+    async ChatIndex(req:Request, res:Response)
+    {
+        try
+        {
+            const response = await db('tb_chat_sala').select('*');
+
+            return res.status(200).json(response);
+        }catch(error)
+        {
+            console.log(error);
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
