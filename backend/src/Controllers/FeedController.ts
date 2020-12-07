e456ca79a21da8656e06ee41b8b4d46095c294eb
Lucas Souza <souzacontato18@gmail.com>
2020-12-07 20:53:38 -0300

    feed query update

--- backend/src/Controllers/FeedController.ts
+++ backend/src/Controllers/FeedController.ts
@@ -2,6 +2,12 @@ import {request, Request,Response} from 'express';
 import db from '../Database/connection';
 
 
+/*
+            const perm = await db('tb_user as u').select("user_id","username","p.description").from('tb_user as u')
+            .rightJoin('tb_permission_user as pu', 'pu.fk_user_id', 'u.user_id')
+            .rightJoin('tb_permissions as p', 'p.id_permission', 'pu.fk_permission_id').where('user_id', user_id); 
+*/
+
 class FeedController
 {
 
@@ -9,8 +15,12 @@ class FeedController
     {
         try
         {
-            //feed do app
-            const feed = db('tb_chat_user').select('*');
+            const tag_id = req.body.tag_id;
+            const feed = await db('tb_chat_user as cu').select('cs.name, cs.member_limit, t.name')
+            .from('tb_chat_user as cu')
+            .rightJoin('tb_chat_sala as cs', 'cu.fk_chat_sala_chat_sala_id', 'cs.chat_sala_id')
+            .rightJoin('tb_chat_tags as ct', 'cs.fk_tags_tag_id', 'ct.id_chat_tag')
+            .rightJoin('tb_tags as t', 'ct.fk_tags_tag_id', 't.tag_id');
 
             return res.status(200).json(feed);
         }catch(error)
@@ -20,9 +30,10 @@ class FeedController
 
 
     }
+    
 
 
-    async CreatePost(req:Request, res:Response)
+    async CreateP0ost(req:Request, res:Response)
     {
         const postagem = {
             Donwload_link:req.body.Donwload_link,
